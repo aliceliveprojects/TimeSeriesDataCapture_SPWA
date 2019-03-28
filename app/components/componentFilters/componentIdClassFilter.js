b02a982abe0f80735c527fd79817c2ac9ba2a43e
YusofBandar <ybandar1902@gmail.com>
2019-03-28 19:31:16 +0000

    Fixed major issue with column filter

--- app/components/componentFilters/componentIdClassFilter.js
+++ app/components/componentFilters/componentIdClassFilter.js
@@ -1,15 +1,17 @@
 app.filter('componentIdClassFilter',function(){
     return function(componentId){
-       var filerId = "_";
+       var filterId = "_";
        // filter out no alphanum characters 
         for(var i=0,len=componentId.length;i<len;i++){
             var code = componentId.charCodeAt(i);
             if((code > 47 && code <58) 
-            && (code >64 && code <91) 
-            && (code > 96 && code < 123)){
+            || (code >64 && code <91) 
+            || (code > 96 && code < 123)){
                 filterId += componentId[i];
             }
         }
-        return filerId
+
+      
+        return filterId
     }
 })
